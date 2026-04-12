175f42fa8b4b62df06fe3dab37da137388389956
Ronnie Chan <ronniekkc@gmail.com>
2026-04-12 16:45:03 -0400

    initial commit

---
+++ magnification_utils.py
@@ -0,0 +1,8 @@
+'''
+File: magnification_utils.py
+--------------------------------------
+Student:Ronnie Chan (27206003)
+
+
+This file contains the classes MedicalImagesDataset, MedicalImages and Viz.
+'''
